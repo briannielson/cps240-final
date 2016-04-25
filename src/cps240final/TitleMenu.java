2cd1310350a6886d4a02e2f229577425dff503e7
paramikel <ostrander1994@gmail.com>
2016-04-25 17:55:30 -0400

    updated options scene

--- src/cps240final/TitleMenu.java
+++ src/cps240final/TitleMenu.java
@@ -15,23 +15,19 @@ import javafx.scene.text.FontWeight;
 //https://www.videoblocks.com/videos/motion-backgrounds
 
 public class TitleMenu {
-	public Scene title, theScene;
-	public Image image;
-	public double width, height;
-	public boolean check = false;
-	Button start = new Button("Start Game");
-	Button options = new Button("Options");
-	Button credits = new Button("Credits");
+	public Scene titleScene, optionScene;
+	public Button start = new Button("Start Game");
+	public Button options = new Button("Options");
+	public Button credits = new Button("Credits");
 	
-	public TitleMenu(Scene theScene) {
-		this.theScene = theScene;
-		Group root = new Group();
-		title = new Scene(root);
-		Canvas canvas = new Canvas (512,512);
-		root.getChildren().add(canvas);
+	public TitleMenu() {
+		Group rootT = new Group();
+		titleScene = new Scene(rootT);
+		Canvas canvasT = new Canvas (512,512);
+		rootT.getChildren().add(canvasT);
 		
-		GraphicsContext gc = canvas.getGraphicsContext2D();
-		gc.setFill(Color.WHITE);
+		GraphicsContext gc = canvasT.getGraphicsContext2D();
+		gc.setFill(Color.RED);
 		gc.setStroke(Color.BLACK);
 		gc.setLineWidth(2);
 		Font theFont = Font.font("CHILLER", FontWeight.BOLD, 48);
@@ -47,9 +43,17 @@ public class TitleMenu {
 		butts.setSpacing(10);
 		butts.setPadding(new Insets(0,20,10,20));
 		butts.getChildren().addAll(start,options,credits);
-		root.getChildren().add(butts);
+		rootT.getChildren().add(butts);
 		butts.setLayoutY(100);
 		
+		Group rootO = new Group();
+		optionScene = new Scene(rootO);
+		Canvas canvasO = new Canvas(512,512);
+		rootO.getChildren().add(canvasO);
+		
+	}
+	
+	public void options() {
 
 	}
 	
