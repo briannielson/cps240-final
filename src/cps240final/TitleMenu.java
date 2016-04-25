a4467a5e586050d58a137b6978e00e3e644f841e
briannielson <mappedgraphics@gmail.com>
2016-04-25 01:36:25 -0400

    made it look prettyish

--- src/cps240final/TitleMenu.java
+++ src/cps240final/TitleMenu.java
@@ -13,30 +13,32 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 //https://www.videoblocks.com/videos/motion-backgrounds
-import javafx.stage.Stage;
 
 public class TitleMenu {
+	public Scene title, theScene;
+	public Image image;
+	public double width, height;
 	public boolean check = false;
-
-	public void start(Stage theStage) {
-		
+	Button start = new Button("Start Game");
+	Button options = new Button("Options");
+	Button credits = new Button("Credits");
+	
+	public TitleMenu(Scene theScene) {
+		this.theScene = theScene;
 		Group root = new Group();
-		Scene theScene = new Scene(root);
-		Canvas canvas = new Canvas (400,400);
+		title = new Scene(root);
+		Canvas canvas = new Canvas (512,512);
 		root.getChildren().add(canvas);
 		
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.setFill(Color.WHITE);
 		gc.setStroke(Color.BLACK);
 		gc.setLineWidth(2);
-		Font theFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
-		gc.fillText("ROCK N' ROLLFARE", 60, 50);
-		gc.strokeText("ROCK N' ROLLFARE", 60, 50);
-		
-		Button start = new Button("Start Game");
-		Button options = new Button("Options");
-		Button credits = new Button("Credits");
-		
+		Font theFont = Font.font("CHILLER", FontWeight.BOLD, 48);
+		gc.setFont(theFont);
+		gc.fillText("ROCK N' ROLLFARE", 5, 50);
+		gc.strokeText("ROCK N' ROLLFARE", 5, 50);
+
 		start.setMaxWidth(Double.MAX_VALUE);
 		options.setMaxWidth(Double.MAX_VALUE);
 		credits.setMaxWidth(Double.MAX_VALUE);
@@ -48,13 +50,8 @@ public class TitleMenu {
 		root.getChildren().add(butts);
 		butts.setLayoutY(100);
 		
-		start.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				check = true;
-				System.out.println("test");
-			}
-		});
-		
+
 	}
+	
+
 }
